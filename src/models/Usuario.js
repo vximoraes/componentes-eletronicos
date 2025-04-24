01e6f0f96827c32a8fce0cde4dc587e390747ce9
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-23 20:47:35 -0400

    Adiciona modelo de Usuário com esquema Mongoose

---
+++ src/models/Usuario.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+import mongoosePaginate from "mongoose-paginate-v2"
+
+class Usuario {
+    constructor() {
+        const usuarioSchema = new mongoose.Schema ({
+            nome : { type: String, index : true , required: true },
+            email: { type: String, unique: true , required: true },
+            senha: { type: String, select: false, required: true }
+        })
+    }
+}
