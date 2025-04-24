e7154dd6fafcb46bd66ade11f6a8316045924a57
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-24 13:44:00 -0400

    adiciona modelo de Notificacao com esquema Mongoose

---
+++ src/models/Notificacao.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+import mongoosePaginate from "mongoose-paginate-v2"
+
+class Notificacao {
+    constructor() {
+        const notificacaoSchema = new mongoose.Schema ({
+            mensagem: { type: String, index: true, required: true },
+            data_hora: { type: Date, required: true, default: Date.now },
+            visualizacao: { type: Date, required: false, default: Date.now }
+        })
+    }
+}
