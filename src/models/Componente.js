864523b79d393c1bdc4b0e24c9320f83745ae324
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-24 13:43:47 -0400

    adiciona modelo de Componente com esquema Mongoose

---
+++ src/models/Componente.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+import mongoosePaginate from "mongoose-paginate-v2"
+
+class Componente {
+    constructor() {
+        const componenteSchema = new mongoose.Schema ({
+            nome: { type: String, index: true, required: true },
+            codigo: { type: String, unique: true, required: true },
+            quantidade: { type: Number, required: true },
+            estoque_minimo: { type: Number, required: true },
+            valor_unitario: { type: Number, required: true },
+            descricao: { type: String, required: false },
+            imagem: { type: String, required: false }
+        })
+    }
+}
