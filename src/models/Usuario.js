c2e6a7f6def0751c02c2d21502d16b514d5a148d
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-24 13:45:53 -0400

    removendo espaços do modelo de Usuario

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -4,8 +4,8 @@ import mongoosePaginate from "mongoose-paginate-v2"
 class Usuario {
     constructor() {
         const usuarioSchema = new mongoose.Schema ({
-            nome : { type: String, index : true , required: true },
-            email: { type: String, unique: true , required: true },
+            nome: { type: String, index: true, required: true },
+            email: { type: String, unique: true, required: true },
             senha: { type: String, select: false, required: true }
         })
     }
