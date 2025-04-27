a042f659356a5a2d360da9c4be3760085d953cf1
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-27 14:22:47 -0400

    adiciona campos de localizacao e categoria ao modelo de Componente

--- src/models/Componente.js
+++ src/models/Componente.js
@@ -10,7 +10,15 @@ class Componente {
             estoque_minimo: { type: Number, required: true },
             valor_unitario: { type: Number, required: true },
             descricao: { type: String, required: false },
-            imagem: { type: String, required: false }
+            imagem: { type: String, required: false },
+            localizacao: { type: mongoose.Schema.Types.ObjectId, ref: "localizacao", required: true }, 
+            categoria: { type: mongoose.Schema.Types.ObjectId, ref: "categoria", required: true }
         })
+
+        componenteSchema.plugin(mongoosePaginate);
+
+        this.model = mongoose.model("componente", componenteSchema);
     }
 }
+
+export default new Componente().model
