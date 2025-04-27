bf82f0eb8e6e0a0ec0f7c1d08ff3ed8a58f0e83e
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-27 14:23:14 -0400

    adiciona modelo de Orcamento com esquema Mongoose

--- src/models/Orcamento.js
+++ src/models/Orcamento.js
@@ -1,12 +1,21 @@
 import mongoose from "mongoose"
 import mongoosePaginate from "mongoose-paginate-v2"
 
+// Falta colocar as chaves estrangeiras e array de componente_orcamento
 class Orcamento {
     constructor() {
         const orcamentoSchema = new mongoose.Schema ({
             nome: { type: String, index: true, required: true },
             protocolo: { type: String, unique: true, required: true },
-            descricao: { type: String, required: false }
+            descricao: { type: String, required: false },
+            valor: { type: Number, required: true },
+            componentes: [{ type: mongoose.Schema.Types.ObjectId, ref: "componente_orcamento" }]
         })
+
+        orcamentoSchema.plugin(mongoosePaginate);
+
+        this.model = mongoose.model("orcamento", orcamentoSchema);
     }
 }
+
+export default new Orcamento().model
