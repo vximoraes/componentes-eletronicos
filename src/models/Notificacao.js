cbe6edfb5a2393eea001d36e762b9482d22e1b49
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-27 14:23:09 -0400

    adiciona campo usuario ao modelo de Notificacao com esquema Mongoose

--- src/models/Notificacao.js
+++ src/models/Notificacao.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose"
 import mongoosePaginate from "mongoose-paginate-v2"
 
+// Falta colocar as chaves estrangeiras
 class Notificacao {
     constructor() {
         const notificacaoSchema = new mongoose.Schema ({
             mensagem: { type: String, index: true, required: true },
             data_hora: { type: Date, required: true, default: Date.now },
-            visualizacao: { type: Date, required: false, default: Date.now }
+            visualizacao: { type: Date, required: false, default: Date.now },
+            usuario: { type: mongoose.Schema.Types.ObjectId, ref: "usuario", required: true }
         })
+
+        notificacaoSchema.plugin(mongoosePaginate);
+
+        this.model = mongoose.model("notificacao", notificacaoSchema);
     }
 }
+
+export default new Notificacao().model
