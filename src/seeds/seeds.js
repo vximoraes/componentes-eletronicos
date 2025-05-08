fcab844dd136fb79c307f7e1c4ce578c48eb6f89
vinimoraes <viniciusmoraesvha@gmail.com>
2025-05-08 16:21:06 -0400

    feat: adiciona chamadas para fornecedorSeed e movimentacaoSeed no script de seeds

--- src/seeds/seeds.js
+++ src/seeds/seeds.js
@@ -2,11 +2,15 @@ import DbConnect from "../config/DbConnect.js";
 import categoriaSeed from "./categoriaSeed.js";
 import localizacaoSeed from "./localizacaoSeed.js";
 import componenteSeed from "./componenteSeed.js";
+import fornecedorSeed from "./fornecedorSeed.js";
+import movimentacaoSeed from "./movimentacaoSeed.js";
 
 DbConnect.conectar();
 
 await categoriaSeed();
 await localizacaoSeed();
 await componenteSeed();
+await fornecedorSeed();
+await movimentacaoSeed();
 
 DbConnect.desconectar();
