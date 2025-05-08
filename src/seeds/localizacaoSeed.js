6b9d1d3227ec397f23afc88af979c1fb96876d3c
vinimoraes <viniciusmoraesvha@gmail.com>
2025-05-08 16:20:45 -0400

    refactor: renomeia função errada de categoriaSeed para localizacaoSeed

--- src/seeds/localizacaoSeed.js
+++ src/seeds/localizacaoSeed.js
@@ -1,7 +1,7 @@
 import { fakeMappings } from "./globalFakeMapping.js";
 import Localizacao from "../models/Localizacao.js";
 
-export default async function categoriaSeed() {
+export default async function localizacaoSeed() {
     await Localizacao.deleteMany({});
 
     for (let i = 0; i < 10; i++) {
