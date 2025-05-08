fedd5070682200b2214050485e369289ebce2ef8
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-05-07 22:31:55 -0400

    feat: implementa rotas de usuários com middleware de autenticação e controle

---
+++ src/routes/usuarioRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+// import AuthMiddleware from "../middlewares/AuthMiddleware.js";
+// import authPermission from '../middlewares/AuthPermission.js';
+// import UsuarioController from '../controllers/UsuarioController.js';
+import { asyncWrapper } from '../utils/helpers/index.js';
+
+const router = express.Router();
+
+const usuarioController = new UsuarioController(); 
+
+router
+    .get("/usuarios", AuthMiddleware, authPermission, asyncWrapper(usuarioController.listar.bind(usuarioController)))
+    .get("/usuarios/:id", AuthMiddleware, authPermission, asyncWrapper(usuarioController.listar.bind(usuarioController)))
+    .post("/usuarios", AuthMiddleware, authPermission, asyncWrapper(usuarioController.criar.bind(usuarioController)))
+    .patch("/usuarios/:id", AuthMiddleware, authPermission, asyncWrapper(usuarioController.atualizar.bind(usuarioController)))
+    .put("/usuarios/:id", AuthMiddleware, authPermission, asyncWrapper(usuarioController.atualizar.bind(usuarioController)))
+    .delete("/usuarios/:id", AuthMiddleware, authPermission, asyncWrapper(usuarioController.deletar.bind(usuarioController)))
+
+export default router;
