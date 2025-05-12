c50b29df7644957c97cc302fabf319f9a20dfb14
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-05-12 16:19:09 -0400

    refactor: remove comentários e imports não utilizados

--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -1,5 +1,3 @@
-// src/utils/helpers/index.js
-
 export { default as CommonResponse } from './CommonResponse.js';
 export { default as CustomError } from './CustomError.js';
 export { default as HttpStatusCodes } from './HttpStatusCodes.js';
@@ -7,11 +5,3 @@ export { default as errorHandler } from './errorHandler.js';
 export { default as messages } from './messages.js';
 export { default as StatusService } from './StatusService.js';
 export { default as asyncWrapper } from '../../middlewares/asyncWrapper.js';
-
-// Adicione outros exports conforme necessário
-
-/* Formas de importar
-   import { CommonResponse, CustomError, HttpStatusCodes, errorHandler, messages, StatusService, asyncWrapper } from '../utils/helpers/index.js';
-   import * as Helpers from '../utils/helpers/index.js';
-   import { CommonResponse as CR } from '../utils/helpers/index.js';
-*/
