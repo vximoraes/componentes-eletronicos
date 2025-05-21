09d1d13c177cb5fdc6d5456a0b17b0dba03205a7
vinimoraes <viniciusmoraesvha@gmail.com>
2025-05-21 18:00:35 -0400

    feat: implementa método de criação de componente e validação para verificar nome de componente em ComponenteService

--- src/services/ComponenteService.js
+++ src/services/ComponenteService.js
@@ -13,6 +13,29 @@ class ComponenteService {
 
         return data;
     }
+
+    async criar(parsedData) {
+        await this.validateNome(parsedData.nome)
+
+        const data = await this.repository.criar(parsedData);
+
+        return data;
+    }
+
+    // Métodos auxiliares.
+
+    async validateNome(nome, id = null) {
+        const componenteExistente = await this.repository.buscarPorNome(nome, id);
+        if (componenteExistente) {
+            throw new CustomError({
+                statusCode: HttpStatusCodes.BAD_REQUEST.code,
+                errorType: 'validationError',
+                field: 'nome',
+                details: [{ path: 'nome', message: 'Nome já está em uso.' }],
+                customMessage: 'Nome já está em uso.',
+            });
+        }
+    }
 }
 
 export default ComponenteService;
