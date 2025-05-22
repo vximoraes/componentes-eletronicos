d5d2c375ce54f2de21f60c82ee50e7c68a602528
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-05-21 20:12:17 -0400

    feat: implementa método atualizar e ensureComponentExists em ComponenteService

--- src/services/ComponenteService.js
+++ src/services/ComponenteService.js
@@ -22,6 +22,15 @@ class ComponenteService {
         return data;
     }
 
+    async atualizar(id, parsedData) {
+        await this.ensureComponentExists(id);
+        await this.validateNome(parsedData.nome)
+
+        const data = await this.repository.atualizar(id, parsedData);
+
+        return data;
+    }
+
     // Métodos auxiliares.
 
     async validateNome(nome, id = null) {
@@ -35,6 +44,21 @@ class ComponenteService {
                 customMessage: 'Nome já está em uso.',
             });
         }
+    }
+
+    async ensureComponentExists(id) {
+        const componenteExistente = await this.repository.buscarPorId(id);
+        if (!componenteExistente) {
+            throw new CustomError({
+                statusCode: 404,
+                errorType: 'resourceNotFound',
+                field: 'Componente',
+                details: [],
+                customMessage: messages.error.resourceNotFound('Componente'),
+            });
+        }
+
+        return componenteExistente;
     }
 }
 
