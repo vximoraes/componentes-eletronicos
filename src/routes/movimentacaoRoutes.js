3e4a5c8e42214b91f5ecd20fedbd8823dfc1dc5f
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-05-22 22:35:29 -0400

    feat: habilita rota POST para criação de movimentações no MovimentacaoRoutes

--- src/routes/movimentacaoRoutes.js
+++ src/routes/movimentacaoRoutes.js
@@ -11,9 +11,7 @@ const movimentacaoController = new MovimentacaoController();
 router
     .get("/movimentacoes", /*AuthMiddleware, authPermission,*/ asyncWrapper(movimentacaoController.listar.bind(movimentacaoController)))
     .get("/movimentacoes/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(movimentacaoController.listar.bind(movimentacaoController)))
-    // .post("/movimentacoes", /*AuthMiddleware, authPermission,*/ asyncWrapper(movimentacaoController.criar.bind(movimentacaoController)))
-    // .patch("/movimentacoes/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(movimentacaoController.atualizar.bind(movimentacaoController)))
-    // .put("/movimentacoes/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(movimentacaoController.atualizar.bind(movimentacaoController)))
+    .post("/movimentacoes", /*AuthMiddleware, authPermission,*/ asyncWrapper(movimentacaoController.criar.bind(movimentacaoController)))
     // .delete("/movimentacoes/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(movimentacaoController.deletar.bind(movimentacaoController)))
 
 export default router;
