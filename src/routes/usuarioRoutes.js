a750dc62895214b70207200a2ecc75428acefdf3
vinimoraes <viniciusmoraesvha@gmail.com>
2025-06-24 15:55:40 -0400

    fix: remove AuthMiddleware de POST /usuarios

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -10,7 +10,7 @@ const usuarioController = new UsuarioController();
 router
     .get("/usuarios", AuthMiddleware, asyncWrapper(usuarioController.listar.bind(usuarioController)))
     .get("/usuarios/:id", AuthMiddleware, asyncWrapper(usuarioController.listar.bind(usuarioController)))
-    .post("/usuarios", AuthMiddleware, asyncWrapper(usuarioController.criar.bind(usuarioController)))
+    .post("/usuarios", asyncWrapper(usuarioController.criar.bind(usuarioController)))
     .patch("/usuarios/:id", AuthMiddleware, asyncWrapper(usuarioController.atualizar.bind(usuarioController)))
     .put("/usuarios/:id", AuthMiddleware, asyncWrapper(usuarioController.atualizar.bind(usuarioController)))
     .delete("/usuarios/:id", AuthMiddleware, asyncWrapper(usuarioController.deletar.bind(usuarioController)))
