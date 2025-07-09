faa78a924fd74c6d1fe76f24fc622849786382fe
Geovanna <rochageovanna3@gmail.com>
2025-07-08 21:50:48 -0400

    alterações routes

--- src/routes/componenteOrcamentoRoutes.js
+++ src/routes/componenteOrcamentoRoutes.js
@@ -1,19 +1,19 @@
 import express from "express";
-// import AuthMiddleware from "../middlewares/AuthMiddleware.js";
-// import authPermission from '../middlewares/AuthPermission.js';
-import OrcamentoController from "../controllers/OrcamentoController.js";
+import AuthMiddleware from "../middlewares/AuthMiddleware.js";
+import authPermission from '../middlewares/AuthPermission.js';
+import ComponenteOrcamentoController from "../controllers/ComponenteOrcamentoController.js";
 import { asyncWrapper } from '../utils/helpers/index.js';
 
 const router = express.Router();
 
-const ComponenteOrcamentoController = new ComponenteOrcamentoController();
+const componenteController = new ComponenteOrcamentoController();
 
 router
-    .get("/componente_orcamentos", /*AuthMiddleware, authPermission,*/ asyncWrapper(componenteController.listar.bind(componenteController)))
-    .get("/componente_orcamentos/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(componenteController.listar.bind(componenteController)))
-    .post("/componente_orcamentos", /*AuthMiddleware, authPermission,*/ asyncWrapper(componenteController.criar.bind(componenteController)))
-    .patch("/componente_orcamentos/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(componenteController.atualizar.bind(componenteController)))
-    .put("/componente_orcamentos/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(componenteController.atualizar.bind(componenteController)))
-    .delete("/componente_orcamentos/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(componenteController.deletar.bind(componenteController)))
+    .get("/componente_orcamentos", AuthMiddleware, authPermission, asyncWrapper(componenteController.listar.bind(componenteController)))
+    .get("/componente_orcamentos/:id", AuthMiddleware, authPermission, asyncWrapper(componenteController.listar.bind(componenteController)))
+    .post("/componente_orcamentos", AuthMiddleware, authPermission, asyncWrapper(componenteController.criar.bind(componenteController)))
+    .patch("/componente_orcamentos/:id", AuthMiddleware, authPermission, asyncWrapper(componenteController.atualizar.bind(componenteController)))
+    .put("/componente_orcamentos/:id", AuthMiddleware, authPermission, asyncWrapper(componenteController.atualizar.bind(componenteController)))
+    .delete("/componente_orcamentos/:id", AuthMiddleware, authPermission, asyncWrapper(componenteController.deletar.bind(componenteController)))
 
 export default router;
